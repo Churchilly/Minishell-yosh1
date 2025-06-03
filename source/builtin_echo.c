dc8b51e8be0b1e82c6d744aeb2dcee9573bc42b7
churchilly <chillywq@gmail.com>
2025-06-03 16:17:28 +0300

    builtin fixed

--- source/builtin_echo.c
+++ source/builtin_echo.c
@@ -6,27 +6,40 @@
 /*   By: yusudemi <yusudemi@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 19:00:21 by yusudemi          #+#    #+#             */
-/*   Updated: 2025/05/31 19:00:47 by yusudemi         ###   ########.fr       */
+/*   Updated: 2025/06/03 16:16:25 by yusudemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "str.h"
 
-int	builtin_echo(char **args)
+// for -n -nnnn -nnnnnnnnn returns 1 else 0
+int	is_ignore_newline(char *str)
 {
 	int	i;
-	int	j;
-	int	newline;
 
-	i = 1;
-	j = 0;
-	newline = 1;
-	if (args[i] && ft_strcmp(args[i], "-n"))
+	i = 0;
+	if (!str)
+		return (0);
+	if (str[i] != '-')
+		return (0);
+	i++;
+	while (str[i])
 	{
-		newline = 0;
+		if (str[i] != 'n')
+			return (0);
 		i++;
 	}
+	return (1);
+}
+
+int	print_args(char **args)
+{
+	int	j;
+	int	i;
+
+	j = 0;
+	i = 0;
 	while (args[i])
 	{
 		if (j)
@@ -36,6 +49,26 @@ int	builtin_echo(char **args)
 		i++;
 		j = 1;
 	}
+	return (0);
+}
+
+int	builtin_echo(char **args)
+{
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	while (args[i])
+	{
+		if (is_ignore_newline(args[i]))
+			newline = 0;
+		else
+			break ;
+		i++;
+	}
+	if(print_args(args + i) == 1)
+		return (1);
 	if (newline)
 		if (write(STDOUT_FILENO, "\n", 1) == -1)
 			return (1);
